d0c19e8c7e073b26908bb13a5fb6ae99aac28ea9
ales27pm <56891139+ales27pm@users.noreply.github.com>
2026-01-29 15:08:24 -0500

    Fix ScrollToTop runtime require

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route } from "wouter";
+import { ScrollToTop } from "@/components/ScrollToTop";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,26 +9,10 @@ import Home from "@/pages/Home";
 import Portfolio from "@/pages/Portfolio";
 import Services from "@/pages/Services";
 import Contact from "@/pages/Contact";
-import { ScrollToTop } from "@/components/ScrollToTop";
-
-// ScrollToTop component logic needs to be a real component or just a useEffect
-// I'll make a small inline component for it here since it wasn't generated separately
-function ScrollHandler() {
-  const { useEffect } =  require("react");
-  const { useLocation } = require("wouter");
-  const [pathname] = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
-
 function Router() {
   return (
     <>
-      <ScrollHandler />
+      <ScrollToTop />
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/projects" component={Portfolio} />
